19a92770f24c623bdf9f259924b519cebc4c141f
Mohammad <moshafie@eng.ucsd.edu>
2022-04-28 21:49:47 -0700

    fix

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-export PATH=/root/anaconda3/bin:$PATH
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/root/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/root/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/root/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
 source ~/.bashrc
 cd /mshvol2/users/mohammad/optimization/burst-denoising-forked
 cp /root/ssh_mount/id_rsa* /root/.ssh/
