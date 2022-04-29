7401925b7bf46e861f7ddcc6c3b684ac28d94898
Mohammad <moshafie@eng.ucsd.edu>
2022-04-28 19:07:39 -0700

    added requirements yaml file

--- experiments/run.sh
+++ experiments/run.sh
@@ -11,7 +11,8 @@ chmod 400 ~/.ssh/id_rsa
 # python3.7 -m pip install --upgrade pip
 # python3.7 -m pip install setuptools 
 # python3.7 -m pip install imageio tensorflow-gpu==1.15.0 scikit-image==0.16.2 tqdm PyExifTool piq lpips plotly==5.6.0 pandas kaleido
-pip install wandb natsort matplotlib scipy numpy tensorflow-gpu==1.13.1 plotly pandas kaleido scikit-image opencv-python==4.2.0.32 tensorboardX jaxlib jax torch
+# pip install wandb natsort matplotlib scipy numpy tensorflow-gpu==1.13.1 plotly pandas kaleido scikit-image opencv-python==4.2.0.32 tensorboardX jaxlib jax torch
+conda env create -f req.yml
 python -c """import imageio
 imageio.plugins.freeimage.download()
 """
